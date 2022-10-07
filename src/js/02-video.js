51acbd61b4150d9c99d8a4164148f6c0714fbe8a
Yaroslava-Vorobets <slavavor85@gmail.com>
2022-10-07 13:31:39 +0200

    add  files

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,21 +3,23 @@ import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
-const player = new Vimeo.Player(iframe);
+const player = new Player(iframe);
 const LOCALSTORAGE_KEY = "videoplayer-current-time";
   
-Update()  
+
 
 player.on('timeupdate', throttle(onTimeSet, 1000));
-
-function onTimeSet({ seconds }) {
+ 
+function onTimeSet({ seconds }) {   
+     console.log(seconds) 
     localStorage.setItem(LOCALSTORAGE_KEY, seconds)
 }  
 
 function Update() {
     const savedTimeSet = localStorage.getItem('LOCALSTORAGE_KEY')
-        if ( !savedTimeSet === null) {
-            player.setCurrentTime(savedTimeSet)
-             console.log(savedTimeSet)
-      }
- }
+    if (savedTimeSet) {            
+            player.setCurrentTime(savedTimeSet) 
+         console.log(savedTimeSet)    
+    }
+} 
+  
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -33,9 +33,10 @@ function upDatePage() {
 const formSubmit = e => {
     e.preventDefault();
     const {
-        elements: { name, message }, } = e.currentTarget;    
-    console.log({ name: name, message: message.value });
+        elements: { email, message }, } = e.currentTarget;    
+    console.log({ email: email.value, message: message.value });
     e.currentTarget.reset();
     remove(LOCALSTORAGE_KEY);
 }
-form.addEventListener ('submit', formSubmit)
+form.addEventListener('submit', formSubmit)
+console.log()
